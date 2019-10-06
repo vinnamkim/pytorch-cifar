fdcf4424bbb0f36107676f1f1fd0763581f9e7e7
vinnamkim <vinnam.kim@gmail.com>
2019-10-07 08:18:57 +0900

    Fix ws
    Signed-off-by: vinnamkim <vinnam.kim@gmail.com>

--- my_models/ws_resnet.py
+++ my_models/ws_resnet.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 class WsConv2d(nn.Conv2d):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1,
@@ -16,9 +15,7 @@ class WsConv2d(nn.Conv2d):
         weight = weight - weight_mean
         std = weight.view(weight.size(0), -1).std(dim=1).view(-1, 1, 1, 1) + 1e-5
         weight = weight / std.expand_as(weight)
-        return F.conv2d(x, weight, self.bias, self.stride,
-                        self.padding, self.dilation, self.groups)
-
+        return self.conv2d_forward(x, weight)
 
 def BatchNorm2d(num_features):
     return nn.GroupNorm(num_channels=num_features, num_groups=32)
