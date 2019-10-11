3ccf568804ae0e08ad864abd276de8d5b3634170
vinnamkim <vinnam.kim@gmail.com>
2019-10-11 15:38:13 +0900

    Make compatible with old pytorch
    Signed-off-by: vinnamkim <vinnam.kim@gmail.com>

--- my_models/ws_resnet.py
+++ my_models/ws_resnet.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.nn import functional as F
 
 class WsConv2d(nn.Conv2d):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1,
@@ -15,7 +16,8 @@ class WsConv2d(nn.Conv2d):
         weight = weight - weight_mean
         std = weight.view(weight.size(0), -1).std(dim=1).view(-1, 1, 1, 1) + 1e-5
         weight = weight / std.expand_as(weight)
-        return self.conv2d_forward(x, weight)
+        return F.conv2d(x, weight, self.bias, self.stride,
+                        self.padding, self.dilation, self.groups)
 
 class WcConv2d(nn.Conv2d):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1,
@@ -29,7 +31,8 @@ class WcConv2d(nn.Conv2d):
         weight_mean = weight.mean(dim=1, keepdim=True).mean(dim=2,
                                   keepdim=True).mean(dim=3, keepdim=True)
         weight = weight - weight_mean
-        return self.conv2d_forward(x, weight)
+        return F.conv2d(x, weight, self.bias, self.stride,
+                        self.padding, self.dilation, self.groups)
         
 def BatchNorm2d(num_features):
     return nn.GroupNorm(num_channels=num_features, num_groups=32)
