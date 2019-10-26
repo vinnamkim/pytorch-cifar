7836384bc540eb62b78f41c416248c3c2a3dc26a
vinnamkim <vinnam.kim@gmail.com>
2019-10-27 01:09:36 +0900

    Change to inplace
    Signed-off-by: vinnamkim <vinnam.kim@gmail.com>

--- imagenet_models/utils.py
+++ imagenet_models/utils.py
@@ -7,6 +7,6 @@ def zerocenter(x):
 def zeronorm(x):
     """x : [B, C, H, W]"""
     """x_mean : [B, 1, 1, 1]"""
-    mean = x.flatten(1).mean(1, keepdim=True).unsqueeze(-1).unsqueeze(-1)
-    std = x.flatten(1).std(1, keepdim=True).unsqueeze(-1).unsqueeze(-1)
+    mean = x.flatten_(1).mean_(1, keepdim=True).unsqueeze_(-1).unsqueeze_(-1)
+    std = x.flatten_(1).std_(1, keepdim=True).unsqueeze_(-1).unsqueeze_(-1)
     return (x - mean) / (std + EPS)
