709d8cc3d254e6ff1db4b6404416e8211fc995d2
vinnamkim <vinnam.kim@gmail.com>
2019-10-30 12:22:50 +0900

    Modify run script for batch size
    Signed-off-by: vinnamkim <vinnam.kim@gmail.com>

--- cifar100_run.sh
+++ cifar100_run.sh
@@ -5,5 +5,5 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 for i in {10..14}
 do
    echo "Run $i model : $1"
-   python $SCRIPT_DIR/main.py --dataset=CIFAR100 --model=$1 --random-seed=$i --epoch=90 --num-workers=$2
+   python $SCRIPT_DIR/main.py --dataset=CIFAR100 --model=$1 --random-seed=$i --epoch=90 --num-workers=$2 --batch-size=$3
 done
