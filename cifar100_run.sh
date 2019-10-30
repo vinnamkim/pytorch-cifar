5ced1228325977c4aa8696b5b76d81bac783c827
vinnamkim <vinnam.kim@gmail.com>
2019-10-30 12:39:44 +0900

    Fix script
    Signed-off-by: vinnamkim <vinnam.kim@gmail.com>

--- cifar100_run.sh
+++ cifar100_run.sh
@@ -4,9 +4,9 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 for i in {10..14}
 do
-   if [${i} -eq 10];then
-      save="-s"
+   echo "Run $i model : $1"
+   if [ ${i} -eq 10 ];then
+      python $SCRIPT_DIR/main.py --dataset=CIFAR100 --model=$1 --random-seed=$i --epoch=90 --num-workers=$2 --batch-size=$3 --save
    fi
-   echo "Run $i model : $1 save : $save"
-   python $SCRIPT_DIR/main.py --dataset=CIFAR100 --model=$1 --random-seed=$i --epoch=90 --num-workers=$2 --batch-size=$3 $save
+   python $SCRIPT_DIR/main.py --dataset=CIFAR100 --model=$1 --random-seed=$i --epoch=90 --num-workers=$2 --batch-size=$3
 done
