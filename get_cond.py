94ae45cedca773302442257336040452d39197f7
vinnamkim <vinnam.kim@gmail.com>
2019-11-01 20:50:15 +0900

    Add analysis
    Signed-off-by: vinnamkim <vinnam.kim@gmail.com>

--- get_cond.py
+++ get_cond.py
@@ -1,4 +1,7 @@
 '''Train CIFAR10 with PyTorch.'''
+from hooks import add_hooks, init_cond_stats, add_cond_stats
+from sklearn.model_selection import StratifiedKFold
+from datasets import get_datasets
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -28,64 +31,63 @@ num_workers = args.num_workers
 if os.name == 'nt':
     num_workers = 0
 
-from datasets import get_datasets
 trainset, trainloader, testset, testloader, num_classes = get_datasets(
     args.dataset, args.batch_size, num_workers)
 
 # Model
 print('==> Building model..')
-#from torchvision.models import resnet18
+#from torchvision.models import resnet50
 if args.model == 'lasso':
-    from my_models.lasso_resnet import resnet18
-    net = resnet18(num_classes=num_classes)
+    from my_models.lasso_resnet import resnet50
+    net = resnet50(num_classes=num_classes)
 elif args.model == 'norm':
-    from my_models.norm_resnet import resnet18
-    net = resnet18(num_classes=num_classes)
+    from my_models.norm_resnet import resnet50
+    net = resnet50(num_classes=num_classes)
 elif args.model == 'norm2':
-    from my_models.norm2_resnet import resnet18
-    net = resnet18(num_classes=num_classes)
+    from my_models.norm2_resnet import resnet50
+    net = resnet50(num_classes=num_classes)
 elif args.model == 'ws':
-    from my_models.ws_resnet import resnet18
-    net = resnet18(num_classes=num_classes)
+    from my_models.ws_resnet import resnet50
+    net = resnet50(num_classes=num_classes)
 elif args.model == 'zerocenter':
-    from my_models.zerocenter_resnet import resnet18
-    net = resnet18(num_classes=num_classes)
+    from my_models.zerocenter_resnet import resnet50
+    net = resnet50(num_classes=num_classes)
 elif args.model == 'zerocenter2':
-    from my_models.zerocenter2_resnet import resnet18
-    net = resnet18(num_classes=num_classes)
+    from my_models.zerocenter2_resnet import resnet50
+    net = resnet50(num_classes=num_classes)
 elif args.model == 'doublenorm':
-    from my_models.doublenorm_resnet import resnet18
-    net = resnet18(num_classes=num_classes)
+    from my_models.doublenorm_resnet import resnet50
+    net = resnet50(num_classes=num_classes)
 elif args.model == 'doublenorm2':
-    from my_models.doublenorm2_resnet import resnet18
-    net = resnet18(num_classes=num_classes)
+    from my_models.doublenorm2_resnet import resnet50
+    net = resnet50(num_classes=num_classes)
 elif args.model == 'doublenorm3':
-    from my_models.doublenorm3_resnet import resnet18
-    net = resnet18(num_classes=num_classes)
+    from my_models.doublenorm3_resnet import resnet50
+    net = resnet50(num_classes=num_classes)
 elif args.model == 'doublenorm4':
-    from my_models.doublenorm4_resnet import resnet18
-    net = resnet18(num_classes=num_classes)
+    from my_models.doublenorm4_resnet import resnet50
+    net = resnet50(num_classes=num_classes)
 elif args.model == 'doublenorm5':
-    from my_models.doublenorm5_resnet import resnet18
-    net = resnet18(num_classes=num_classes)
+    from my_models.doublenorm5_resnet import resnet50
+    net = resnet50(num_classes=num_classes)
 elif args.model == 'doublenorm6':
-    from my_models.doublenorm6_resnet import resnet18
-    net = resnet18(num_classes=num_classes)
+    from my_models.doublenorm6_resnet import resnet50
+    net = resnet50(num_classes=num_classes)
 elif args.model == 'doublenorm7':
-    from my_models.doublenorm7_resnet import resnet18
-    net = resnet18(num_classes=num_classes)
+    from my_models.doublenorm7_resnet import resnet50
+    net = resnet50(num_classes=num_classes)
 elif args.model == 'avgpoolnorm':
-    from my_models.avgpoolnorm_resnet import resnet18
-    net = resnet18(num_classes=num_classes)
+    from my_models.avgpoolnorm_resnet import resnet50
+    net = resnet50(num_classes=num_classes)
 elif args.model == 'ws_doublenorm':
-    from my_models.ws_doublenorm_resnet import resnet18
-    net = resnet18(num_classes=num_classes)
+    from my_models.ws_doublenorm_resnet import resnet50
+    net = resnet50(num_classes=num_classes)
 elif args.model == 'wc_doublenorm':
-    from my_models.wc_doublenorm_resnet import resnet18
-    net = resnet18(num_classes=num_classes)
+    from my_models.wc_doublenorm_resnet import resnet50
+    net = resnet50(num_classes=num_classes)
 
 print('num_classes : ', num_classes)
-dir_name = args.model + '_18_' + str(args.batch_size)
+dir_name = args.model + '_50_' + str(args.batch_size)
 
 if args.random_seed is not None:
     dir_name += '_' + str(args.random_seed)
@@ -106,41 +108,43 @@ if args.resume:
 
 criterion = nn.CrossEntropyLoss()
 
-from sklearn.model_selection import StratifiedKFold
-fold = StratifiedKFold(n_splits=500, shuffle=True, random_state=args.random_seed)
+fold = StratifiedKFold(n_splits=500, shuffle=True,
+                       random_state=args.random_seed)
 splits = fold.split(trainset.data, trainset.targets)
 
 datasets = [s[1] for i, s in enumerate(splits) if i < 5]
-    
+
 for batch in datasets:
     inputs = torch.stack([trainset[i][0] for i in batch])
     outputs = torch.tensor([trainset[i][1] for i in batch])
 
-from hooks import add_hooks, init_cond_stats, add_cond_stats
 
 add_hooks(net)
 
 results = {}
 
-for epoch in range(5, 91, 5):
-    checkpoint = torch.load(os.path.join(dir_name, 'ckpt_{0}.pth'.format(epoch)))
+epochs = [i for i in range(1, 6)] + [i for i in range(10, 91, 10)]
+for epoch in epochs:
+    checkpoint = torch.load(os.path.join(
+        args.prefix, dir_name, 'ckpt_{0}.pth'.format(epoch)))
     net.load_state_dict(checkpoint)
     print('epoch {0} loaded batch_size : {1}'.format(epoch, args.batch_size))
-    
+
     stats = init_cond_stats(net)
 
     for step, batch in enumerate(trainloader):
         inputs, targets = batch
-        
+
         net.zero_grad()
         loss = criterion(net(inputs.cuda()), targets.cuda())
         loss.backward()
         add_cond_stats(net, stats)
-        
+
         if step > 300 / args.batch_size:
             break
-    
+
     print('epoch {0} done'.format(epoch))
     results[epoch] = stats
 
-torch.save(results, os.path.join(dir_name, 'forwards_backwards_{0}.stats'.format(args.batch_size)))
+torch.save(results, os.path.join(args.prefix, dir_name,
+                                 'forwards_backwards_{0}.stats'.format(args.batch_size)))
--- hooks.py
+++ hooks.py
@@ -1,11 +1,13 @@
 import torch
 from torch import nn
 
+
 def backward_hook(module, grad_input, grad_output):
     #print(module, gradInput, gradOutput)
     #print(copy.deepcopy('{0}'.format(str(i))), module)
     module.grad_inputs = grad_input[0].data.clone()
     module.grad_outputs = grad_output[0].data.clone()
+
 
 def forward_hook(module, m_input, m_output):
     #print(module, gradInput, gradOutput)
@@ -13,18 +15,22 @@ def forward_hook(module, m_input, m_output):
     module.m_inputs = m_input[0].data.clone()
     module.m_outputs = m_output[0].data.clone()
 
+
 def add_hooks(net):
     for name, module in net.named_modules():
         if 'layer' in name and 'conv' in name:
             module.register_forward_hook(forward_hook)
             module.register_backward_hook(backward_hook)
 
+
 def init_cond_stats(net):
     stats = {
         'forwards_cond': {},
         'backwards_cond': {},
         'forwards_fbnorm': {},
-        'backwards_fbnorm': {}
+        'backwards_fbnorm': {},
+        'forwards_shape': {},
+        'backwards_shape': {}
     }
     for name, _ in net.named_modules():
         if 'layer' in name and 'conv' in name:
@@ -33,23 +39,26 @@ def init_cond_stats(net):
 
     return stats
 
+
 def add_cond_stats(net, stats):
     with torch.no_grad():
         for name, module in net.named_modules():
-            if 'layer' in name and 'conv' in name:
+            if 'layer' in name and 'conv' in name and module.kernel_size == (1, 1):
                 inputs = module.m_inputs.flatten(2)
                 grads = module.grad_outputs.flatten(2)
-                
+
                 # I = inputs.bmm(inputs.transpose(1, 2))
                 # G = grads.bmm(grads.transpose(1, 2))
-                
+
                 _, s1, _ = inputs.svd(compute_uv=False)
                 _, s2, _ = grads.svd(compute_uv=False)
 
-                forward_cond_nums = (s1[:, 0].log() - s1[:, -1].log()).detach().cpu()
-                backward_cond_nums = (s2[:, 0].log() - s2[:, -1].log()).detach().cpu()
-                forwards_fbnorm = torch.norm(inputs, p='fro', dim=(1,2))
-                backwards_fbnorm = torch.norm(grads, p='fro', dim=(1,2))
+                forward_cond_nums = (
+                    s1[:, 0].log() - s1[:, -1].log()).detach().cpu()
+                backward_cond_nums = (
+                    s2[:, 0].log() - s2[:, -1].log()).detach().cpu()
+                forwards_fbnorm = torch.norm(inputs, p='fro', dim=(1, 2))
+                backwards_fbnorm = torch.norm(grads, p='fro', dim=(1, 2))
 
                 #     for g in grads:
                 #         _, s, _ = g.svd(compute_uv=False)
@@ -60,3 +69,5 @@ def add_cond_stats(net, stats):
                 stats['backwards_cond'][name].append(backward_cond_nums)
                 stats['forwards_fbnorm'][name].append(forwards_fbnorm)
                 stats['backwards_fbnorm'][name].append(backwards_fbnorm)
+                stats['forwards_shape'][name] = inputs.size()[1:]
+                stats['backwards_shape'][name] = grads.size()[1:]
